86880524b93ce620d75f5d809a7663abf2f6a1d6
satyam235 <satyamshukla95183@gmail.com>
2025-11-21 11:48:42 +0530

    Adding further fixes

--- SecOpsRebootNotifier/SecOpsRebootNotifier/SecOpsRebootNotifier/PanelController.swift
+++ SecOpsRebootNotifier/SecOpsRebootNotifier/SecOpsRebootNotifier/PanelController.swift
@@ -7,7 +7,7 @@ class PanelController: NSObject {
     private var timer: DispatchSourceTimer?
     
     // MARK: - Panel geometry - hardcoded values
-    private let panelWidth: CGFloat = 380  // Increased to accommodate countdown + options
+    private let panelWidth: CGFloat = 340  // Optimized to fit countdown + options
     private let topMargin: CGFloat = 16    // Hardcoded top margin as requested
     private let rightMargin: CGFloat = 24  // Hardcoded right margin as requested
     private let cornerRadius: CGFloat = 16
